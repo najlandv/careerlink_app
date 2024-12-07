e7c0f9e0b214d3baf918a6d89a76a06b5eba1b2a
najlandv <nadiva979@gmail.com>
2024-12-07 16:21:05 +0700

    component card

--- app/src/main/java/com/example/careerlink/frontend/component/CardRiview.kt
+++ app/src/main/java/com/example/careerlink/frontend/component/CardRiview.kt
@@ -2,18 +2,12 @@ package com.example.careerlink.frontend.component
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -25,43 +19,22 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun CardAction(title: String="Judul Besar", subtitle: String="Sub judul :", desk: String="Siapa namanya dimana rumahnya nama saya firman syahputra", date: String="12-12-1212"
-
-               ) {
+fun CardRiview(username: String="Rizkaa22", desk: String="Gimana komentarnya magang disana asik kah??", date: String="12-12-1212") {
     Card(modifier = Modifier.padding(16.dp)
         .fillMaxWidth(),
         colors = CardDefaults.cardColors(Color(0xFFFFDE59)),
     ) {
         Column(modifier = Modifier.padding(24.dp),
             verticalArrangement = Arrangement.SpaceBetween) {
-            Column() {
-                Row(modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceBetween) {
-                    Text(title,
-                        style = TextStyle(
-                            fontWeight = FontWeight.Bold,
-                            fontSize = 24.sp,
-                            color = Color.White
-                        )
-                    )
-                    Row {
-                        Icon(Icons.Default.Edit, contentDescription = "",
-                            tint = Color.Blue
-                        )
-                        Spacer(modifier = Modifier.width(8.dp))
-                        Icon(Icons.Default.Delete, contentDescription = "",
-                            tint = Color.Red
-                        )
-
-                    }
-                }
-                Spacer(modifier = Modifier.height(24.dp))
-                Text(subtitle,
+            Column {
+                Text(username,
                     style = TextStyle(
-                        fontWeight = FontWeight.W500,
-                        fontSize = 16.sp
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 24.sp,
+                        color = Color.White
                     )
                 )
+                Spacer(modifier = Modifier.height(24.dp))
                 Text(desk,
                     style = TextStyle(
                         fontWeight = FontWeight.W500,
@@ -83,6 +56,6 @@ fun CardAction(title: String="Judul Besar", subtitle: String="Sub judul :", desk
 
 @Preview(showBackground = true)
 @Composable
-private fun CardActionPrev() {
-    CardAction()
+private fun CardRiviewPrev() {
+    CardRiview()
 }

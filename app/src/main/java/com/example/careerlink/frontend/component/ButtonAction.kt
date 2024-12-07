d330536a127e02abcaba51b955e0ca848fdb62fb
Najla Humaira Desni <najlahumairadesni@gmail.com>
2024-12-07 15:04:02 +0700

    component

---
+++ app/src/main/java/com/example/careerlink/frontend/component/ButtonAction.kt
@@ -0,0 +1,16 @@
+package com.example.careerlink.frontend.component
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ButtonAction(modifier: Modifier = Modifier) {
+
+}
+
+@Preview
+@Composable
+private fun ButtonActionPrev() {
+    ButtonAction()
+}
